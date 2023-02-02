ff5fb1c92581688f2acd26058bcb96c1c350a81b
NataliaCsillag <117922371+NataliaCsillag@users.noreply.github.com>
2023-02-02 14:16:42 +0100

    App fixed

--- src/app.js
+++ src/app.js
@@ -12,47 +12,68 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast () {
+  return days[day];
+
+function displayForecast (response) {
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
-    
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let forecastHTML = `<div class="row">`;
-    days.forEach(function(day) {
-        forecastHTML = forecastHTML + `
+    forecast.forEach(function (forecastDay, index) 
+    {
+        if (index < 6) {
+            forecastHTML = 
+         forecastHTML + `
                      <div class="col-2">
-                        <div class="weather-forecast-date">${day}</div>
+                        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
                         <img 
-                        src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-                        alt=""
-                        width="44"
-                        />
+                        src=http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
                      <div class="weather-forecast-temperature">
-                        <span class="weather-forecast-temperature-max">
-                            6°
+                        <span class="weather-forecast-temperature-max"> ${Math.round(
+                            forecastDay.temp.max
+                        )}
+                            °
                         </span>
-                        <span class="weather-forecast-temperature-min">
-                            -1°
+                        <span class="weather-forecast-temperature-min"> ${
+                        Math.round( forecastDay.temp.min)
+                        }
+                            °
                         </span>
                      </div>
                      </div>
                     
                      `;
+                    }
     });
     
 
 
 forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-    console.log(forecastHTML);
 
+}
+
+function getForecast(coordinates) {
+  let apiKey = "05c2b16e8ce157a0f618d503eb809bb5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
 let cityElement = document.querySelector("#city")    
 let temperatureElement = document.querySelector("#temperature");
-let typeElement = document.querySelector("#type");
+let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
@@ -65,14 +86,16 @@ celsiusTemperature = response.data.main.temp;
 
 temperatureElement.innerHTML = Math.round(celsiusTemperature);
 cityElement.innerHTML = response.data.name;
-typeElement.innerHTML = response.data.weather[0].description;
+descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", 
 `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+getForecast(response.data.coord);
 }
+
 
 function search(city) {
 let apiKey = "05c2b16e8ce157a0f618d503eb809bb5";
@@ -86,35 +109,7 @@ function handleSubmit(event) {
     search(cityInputElement.value)
 }
 
-
-
-function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature); 
-
-}
-
-function displayCelsiusTemperature(event) {;
-event.preventDefault();
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-
-let fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", displayFahrenheitTemperature);
-
-let celsius = document.querySelector("#celsius");
-celsius.addEventListener("click", displayCelsiusTemperature);
-
 search("Gaillard");
-displayForecast()
